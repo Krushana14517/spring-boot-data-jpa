8d48c7cb59fb31495adc0843995e2a914143466a
Krushana14517 <133999441+Krushana14517@users.noreply.github.com>
2025-12-27 23:06:14 +0530

    Delete ManagerRepo.java

--- ManagerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.crud.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.crud.model.ManagerModel;
-
-public interface ManagerRepo extends CrudRepository<ManagerModel, Integer> {
-
-}
