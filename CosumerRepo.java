32f01dad5caab0c142522891dcef830874792a45
Krushana14517 <133999441+Krushana14517@users.noreply.github.com>
2025-12-29 09:59:57 +0530

    Delete CosumerRepo.java

--- CosumerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.crud.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.crud.model.CosumerModel;
-
-public interface CosumerRepo extends CrudRepository<CosumerModel, Integer>{
-
-}
